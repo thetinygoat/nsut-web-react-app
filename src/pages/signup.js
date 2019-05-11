530a0ac0394e9fe45c9c753cf64cce31d402d112
Sachin Saini <iamsainisachin@gmail.com>
2019-05-11 22:27:07 +0530

    add form validation and error messages

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from '../axios';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
@@ -8,52 +8,77 @@ const signup = () => {
 	const [email, setEmail] = useState('');
 	const [password, setPassword] = useState('');
 	const [loggedin, setloggedin] = useState(false);
+	const [error, setError] = useState({ isError: false, reason: '' });
+	const [invalidForm, setFormInvalidity] = useState(true);
+	// submit handler
 	const handleSubmit = async e => {
 		const res = await axios.post('/user/new', {
 			email: email,
 			password: password
 		});
-		if (res.data) {
+		console.log(res.data);
+		// error handler
+		if (!res.data.error) {
 			localStorage.setItem('auth-token', res.data.token);
 			setloggedin(true);
+		} else {
+			setError({ isError: res.data.error, reason: res.data.reason });
 		}
 	};
+	// use effect hook
+	useEffect(() => {
+		const token = localStorage.getItem('auth-token');
+		if (token) {
+			setloggedin(true);
+		}
+		if (email.trim(' ').length > 0 && password.trim(' ').length > 0) {
+			setFormInvalidity(false);
+		} else {
+			setFormInvalidity(true);
+		}
+	}, [email, password]);
 	return (
-		<Card
-			style={{
-				width: '50%',
-				margin: '1em auto',
-				padding: '1em',
-				display: 'flex',
-				flexDirection: 'column'
-			}}
-		>
-			<TextField
-				label="Email"
-				style={{ margin: '1em' }}
-				onChange={e => {
-					setEmail(e.target.value);
+		<React.Fragment>
+			{/* display error message */}
+			{error.isError && <div>{error.reason}</div>}
+			<Card
+				style={{
+					width: '50%',
+					margin: '1em auto',
+					padding: '1em',
+					display: 'flex',
+					flexDirection: 'column'
 				}}
-			/>
-			<TextField
-				label="Password"
-				type="password"
-				style={{ margin: '1em' }}
-				onChange={e => setPassword(e.target.value)}
-			/>
-			<Button
-				variant="contained"
-				color="primary"
-				style={{ margin: '1em' }}
-				onClick={handleSubmit}
 			>
-				Sign up
-			</Button>
-			<p>
-				<Link to="/">Login</Link> instead
-			</p>
-			{loggedin && <Redirect to="/todos" />}
-		</Card>
+				<TextField
+					label="Email"
+					type="email"
+					style={{ margin: '1em' }}
+					onChange={e => {
+						setEmail(e.target.value);
+					}}
+				/>
+				<TextField
+					label="Password"
+					type="password"
+					style={{ margin: '1em' }}
+					onChange={e => setPassword(e.target.value)}
+				/>
+				<Button
+					variant="contained"
+					color="primary"
+					style={{ margin: '1em' }}
+					onClick={handleSubmit}
+					disabled={invalidForm}
+				>
+					Sign up
+				</Button>
+				<p>
+					<Link to="/">Login</Link> instead
+				</p>
+				{loggedin && <Redirect to="/todos" />}
+			</Card>
+		</React.Fragment>
 	);
 };
 
